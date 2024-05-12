09368b10ffd80f76c1f76a978f9ff1b34a0ea28d
metatech-programmer <97856988+metatech-programmer@users.noreply.github.com>
2024-05-12 13:12:22 -0500

    update

--- src/config/conexion/VariablesConexion.ts
+++ src/config/conexion/VariablesConexion.ts
@@ -5,5 +5,5 @@ export default {
   password: process.env.DB_PASSWORD || "1234",
   host: process.env.DB_HOST || "localhost",
   database: process.env.DB_DATABASE || "db_login",
-  port: parseInt(process.env.DB_PORT || "5432"),
+  port: parseInt(process.env.DB_PORT || "5432")
 };
