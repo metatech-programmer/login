91f4a1af2b278e911985fcc6cf6b27c2c91364df
metatech-programmer <97856988+metatech-programmer@users.noreply.github.com>
2024-05-12 19:48:53 -0500

    update vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable no-undef */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    historyApiFallback: true,
+  },
+});
